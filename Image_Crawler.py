8424efd909dbed6ff2b5becab7c39138b1c49da3
Ti-tanium <1158976264@qq.com>
2019-04-01 17:22:46 +0800

    initiate

---
+++ Image_Crawler.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# chenlong
+import re
+import urllib.request
+import os
+import chardet   #需要导入这个模块，检测编码格式
+
+
+# 抓取网页图片
+
+
+# 根据给定的网址来获取网页详细信息，得到的html就是网页的源代码
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    encode_type = chardet.detect(html)  
+    html = html.decode(encode_type['encoding'])
+    return html
+
+
+# 创建保存图片的文件夹
+def mkdir(path):
+    path = path.strip()
+    # 判断路径是否存在
+    # 存在    True
+    # 不存在  Flase
+    isExists = os.path.exists(path)
+    if not isExists:
+        print("Created new folder:",path)
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已经存在
+        return False
+
+
+# 输入文件名，保存多张图片
+def saveImages(imglist, name):
+    global number
+    number=1
+    for imageURL in imglist:
+        splitPath = imageURL.split('.')
+        fTail = splitPath.pop()
+        if len(fTail) > 3:
+            fTail = 'jpg'
+        fileName = name + "/" +"cute_"+str(number) + "." + fTail
+        # 对于每张图片地址，进行保存
+        try:
+            u = urllib.request.urlopen(imageURL)
+            data = u.read()
+            f = open(fileName, 'wb+')
+            f.write(data)
+            print("Saving:",fileName)
+            f.close()
+        except urllib.request.URLError as e:
+            print (e.reason)
+        number += 1
+
+
+        # 获取网页中所有图片的地址
+
+
+def getAllImg(html):
+    # 利用正则表达式把源代码中的图片地址过滤出来
+    reg = r'data-src="(.+?=jpg)"'
+    pattern = re.compile(reg)
+    imglist = pattern.findall(html)  # 表示在整个网页中过滤出所有图片的地址，放在imglist中
+    print(html)
+    print("Len",len(imglist))
+    return imglist
+
+
+# 创建本地保存文件夹，并下载保存图片
+if __name__ == '__main__':
+    style=['cute','fresh','business']
+    for query in style:
+        for i in range(1,400):
+            print("Page:",i)
+            html = getHtml("https://www.freepik.com/search?page="+str(i)+"&query="+query+"&sort=popular&type=photo")  # 获取该网址网页详细信息，得到的html就是网页的源代码
+            path = u'F:\doc\Contests\Intel\Data\'
+            mkdir(path)  # 创建本地文件夹
+            imglist = getAllImg(html)  # 获取图片的地址列表
+            saveImages(imglist, path)  # 保存图片
+    print("Total images count:",number)
